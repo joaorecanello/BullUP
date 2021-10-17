6fe1592b85720bc0855f42b42d2f19bf0f037276
Joao Recanello <joaorecanello@hotmail.com>
2021-10-17 09:33:41 -0300

    Add project files.

---
+++ Gamethon/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Gamethon.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gamethon.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Estudo()
+        {
+            return View();
+        }
+
+        public IActionResult Investimento()
+        {
+            return View();
+        }
+
+        public IActionResult Renda()
+        {
+            return View();
+        }
+
+    }
+}
